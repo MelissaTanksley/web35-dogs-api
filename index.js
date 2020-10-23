e3a7d2c9cdfcda7e2994c19af857ca953ecfb1fe
Melissa Longenberger <melissalongenberger24@gmail.com>
2020-10-23 15:39:42 -0400

    added env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 
 const server = express()
@@ -8,7 +9,8 @@ server.use('/', (req,res) => {
     res.status(200).json({ message: 'Hello Lambda Students'})
 })
 
-const PORT = 5000
+// const PORT = 5000
+const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => {
     console.log(`Server is on port: ${PORT}`)
